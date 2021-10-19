b018cef05df9965d844de004479e0a4607f6889e
Shruti <60984268+shruti2126@users.noreply.github.com>
2021-10-19 14:15:45 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Navigator from './Routes/HomeRoute';
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
